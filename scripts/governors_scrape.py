2c473204f0a97c580f73eaa70ee090e9998bdf25
Trey Hakanson <treyhakanson@gmail.com>
2016-11-19 14:14:42 -0500

    added governors

---
+++ scripts/governors_scrape.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup as bs
+
+base_url = 'https://en.wikipedia.org'
+query_url = 'https://en.wikipedia.org/wiki/List_of_current_United_States_governors'
+html = requests.get(query_url).content
+soup = bs(html, 'lxml')
+
+governors_table = soup.select('table')[1]
+governors_rows = governors_table.find_all('tr')
+governors_rows.pop(0)
+
+header = ['Link']
+for col in governors_rows.pop(0).find_all('th'):
+	if col.get_text() in 'Image':
+		continue
+	header.append(col.get_text())
+
+governors_data = []
+
+for governor in governors_rows:
+	cols = governor.find_all('td')
+	cols.pop(3)
+	cols.pop(1)
+	governor_data = [base_url + cols[1].select('span > span > a')[0]['href']]
+	for col in cols:
+		governor_data.append(col.get_text())
+	governors_data.append(governor_data)
+
+filename = '../governors/governors_data.csv'
+df = pd.DataFrame(governors_data, columns=header)
+df.to_csv(filename, encoding='utf-8', sep='\t')
